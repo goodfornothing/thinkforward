81c638a12abc9cd6dd17c2c7bb6e08190938fed9
Andrew Sprinz <andrewsprinz@gmail.com>
2012-10-26 17:26:37 +0100

    ST

--- vendor/assets/javascripts/active_admin.js
+++ vendor/assets/javascripts/active_admin.js
@@ -10,24 +10,9 @@
 //= require jquery_ujs
 //= require active_admin/application
 
-//= require sir-trevor
-//= require sir-trevor/blocks/heading
-//= require sir-trevor/blocks/markdown
-
 
 $(document).ready(function(){
 		
-		$(function(){
-						
-			if($('.sir-trevor-area').length> 0) {
-		  	
-				var editor = new SirTrevor.Editor({ 
-					el: $('.sir-trevor-area'),
-					blockTypes: ["Text", "Heading", "Quote", "Ul", "Tweet", "Image", "Video"]
-				});
-								
-			}
-			
-		});
+		
 		
 });
