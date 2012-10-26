6d7b71f153953230729086ffb363b20a6e4ca6bf
Andrew Sprinz <andrewsprinz@gmail.com>
2012-10-26 17:13:58 +0100

    ACP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, :with => :application_error
-  end
- 
-  def application_error(exception)
-    ExceptionNotifier::Notifier.exception_notification(request.env, exception).deliver
-  end
-
 end
