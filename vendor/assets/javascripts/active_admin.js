5d81e8bf3213f976d2d2ae4349f909e4b7aebe48
Andrew Sprinz <andrewsprinz@gmail.com>
2012-10-26 17:32:34 +0100

    Hi

--- vendor/assets/javascripts/active_admin.js
+++ vendor/assets/javascripts/active_admin.js
@@ -1,13 +1,4 @@
-//= require jquery
-
-//= require jquery.ui.core
-//= require jquery.ui.widget
-//= require jquery.ui.mouse
-//= require jquery.ui.position
-//= require jquery.ui.tabs
-//= require jquery.ui.datepicker
-
-//= require jquery_ujs
+//= require active_admin/base
 //= require active_admin/application
 
 $(document).ready(function(){
